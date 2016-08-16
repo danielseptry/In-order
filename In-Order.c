f77c37fff0d585c0c9f51ccaabfa1fa04b1f59b0
danielseptry <septrypanjaitan@gmail.com>
2016-08-16 09:00:38 -0700

    add printf

--- In-Order.c
+++ In-Order.c
@@ -17,3 +17,5 @@ void tambah(Node **root, int databaru){
             (*root) = baru; 
             (*root)->kiri = NULL;
             (*root)->kanan = NULL;
+            printf("Data bertambah!");
+      }
